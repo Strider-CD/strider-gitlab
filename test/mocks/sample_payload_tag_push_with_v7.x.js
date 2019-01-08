f7f8d618fe0cc26305e4adc297dde9d01f0d29fc
idler <zqc.sunny@gmail.com>
2019-01-08 10:01:31 +0800

    feat: Tag push support (#110)

---
+++ test/mocks/sample_payload_tag_push_with_v7.x.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  ref: 'refs/tags/tag-1.1.2',
+  before: '9be0948373aedbbed7ac6ce1da708061fac072d7',
+  after: '352e6fe2ea42d394a21dc7995df2116e86bb0684',
+  user_id: 3,
+  user_name: 'Strider Tester',
+  project_id: 1,
+  repository: {
+    name: 'privproject1',
+    url: 'git@nodev:stridertester/privproject1.git',
+    description: 'Test project 1.',
+    homepage: 'http://nodev/stridertester/privproject1'
+  }
+};
